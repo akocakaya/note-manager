90e8b4f083dd37419da4b8ac2d74589aa9a4c3b1
Abdullah Kocakaya <akocakaya@users.noreply.github.com>
2021-02-11 00:11:08 +0300

    mongo db added and connected.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
 const app = express();
+
+mongoose.connect('mongodb://localhost/note-manager-db', {useNewUrlParser: true, useUnifiedTopology: true})
+.then(
+    app.listen(3001, () => {
+        console.log('MongoDb connected');
+        console.log('Note manager app listening on port 3001');
+    })
+).catch(err => {
+    console.log(err);
+});
 
 app.get('/', (req, res) => {
     res.send('Hello world');
 });
 
-app.listen(3001);
+
