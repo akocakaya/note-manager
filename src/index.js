7d2fcaa75c77d062286577776031cdd9d696892e
Abdullah Kocakaya <akocakaya@users.noreply.github.com>
2021-02-11 23:47:23 +0300

    mongo db connection refactor

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import express from 'express';
 import mongoose from 'mongoose';
 
+import connectDb from './db';
+
 import User from './models/user';
 
 const app = express();
 
-mongoose.connect('mongodb://localhost/note-manager-db', {useNewUrlParser: true, useUnifiedTopology: true})
-.then(
+connectDb().then(
     app.listen(3001, () => {
-        console.log('MongoDb connected');
         console.log('Note manager app listening on port 3001');
     })
 ).catch(err => {
